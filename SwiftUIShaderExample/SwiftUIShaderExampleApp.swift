7d4763e27d9824f797a16bf65d524852017db355
H.K <16552133+hdk2200@users.noreply.github.com>
2025-10-26 16:02:51 +0900

    first commit

---
+++ SwiftUIShaderExample/SwiftUIShaderExampleApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2025, ___ORGANIZATIONNAME___ All rights reserved. 
+// 
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
